e8347856d37e82da2c40d621b8765a83de97d476
Rodrigo Martins <rodrigo.matias.martins@live.com>
2024-06-10 17:52:46 -0300

    main page header 2

--- src/App.js
+++ src/App.js
@@ -4,34 +4,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <button id="wharever">
-            <nav id="sidebar-menu">
-             <ul>
-               <li>className="App-link"
-                <a
-                  className="App-link"
-                  href="https://rodrigomartins20.github.io/user-task/"
-                >
-                 MAIN PAGE
-                </a>
-               </li>
-             </ul>
-            </nav>
-        </button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+     <button id="wharever">
+      <nav id="sidebar-menu">
+       <ul>
+        <li>
+         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          href="https://rodrigomartins20.github.io/user-task/"
+         >
+          MAIN PAGE
+         </a>
+        </li>
+       </ul>
+      </nav>
+     </button>
+     <header className="App-header">
+       <img src={logo} className="App-logo" alt="logo" />
+       <p>
+         Edit <code>src/App.js</code> and save to reload.
+       </p>
+       <a
+         className="App-link"
+         href="https://reactjs.org"
+         target="_blank"
+         rel="noopener noreferrer"
+       >
+         Learn React
+       </a>
+     </header>
     </div>
   );
 }
